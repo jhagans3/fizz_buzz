478135ba492f62e15dabcdb13d466b363ddf3fa6
James Hagans II <jhagans3@gmail.com>
2019-01-12 17:25:26 -0500

    update pattern with methods

--- src/main.rs
+++ src/main.rs
@@ -22,25 +22,37 @@ impl FizzBuzz {
     fn multiple_of_5(num: i32) -> bool {
         num % 5 == 0
     }
+
+    fn fizz_buzz_side_effect(&self, num: i32) -> () {
+        println!("{} fizzbuzz", num)
+    }
+
+    fn fizz_side_effect(&self, num: i32) -> () {
+        println!("{} fizz", num)
+    }
+
+    fn buzz_side_effect(&self, num: i32) -> () {
+        println!("{} buzz", num)
+    }
 }
 
 fn fizz_buzz() {
     for number in 1..101 {
         match FizzBuzz::new(number) {
-            FizzBuzz {
+            instance @ FizzBuzz {
                 multiple_of_3: true,
                 multiple_of_5: true,
-            } => println!("{} fizzbuzz", number),
+            } => instance.fizz_buzz_side_effect(number),
 
-            FizzBuzz {
+            instance @ FizzBuzz {
                 multiple_of_3: true,
                 multiple_of_5: false,
-            } => println!("{} fizz", number),
+            } => instance.fizz_side_effect(number),
 
-            FizzBuzz {
+            instance @ FizzBuzz {
                 multiple_of_3: false,
                 multiple_of_5: true,
-            } => println!("{} buzz", number),
+            } => instance.buzz_side_effect(number),
 
             _ => println!("{}", number),
         }
